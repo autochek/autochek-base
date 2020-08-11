cb7145fcb68cb031c5598fe1357b5c5720a75329
chang.lee <chang.lee@credimworks.com>
2020-08-12 02:06:44 +0900

    - 비밀번호 변경 요청 클래스의 현재 비밀번호 속성명 수정 (Password -> CurrentPassword)

--- request/account/request-change-password.model.ts
+++ request/account/request-change-password.model.ts
@@ -1,12 +1,12 @@
 
 
 export class RequestChangePassword {
-	public Password: string;
+	public CurrentPassword: string;
 	public NewPassword: string;
 	public NewConfirmPassword: string;
 
 	constructor(Password?: string, NewPassword?: string, NewConfirmPassword?: string) {
-		this.Password = Password;
+		this.CurrentPassword = Password;
 		this.NewPassword = NewPassword;
 		this.NewConfirmPassword = NewConfirmPassword;
 	}
